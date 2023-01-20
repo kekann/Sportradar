fae25d63a6539465e42a9d681703dc2fae98d640
Jan Zenger <janek.zeng@gmail.com>
2023-01-20 15:16:51 +0100

    aaa

--- index.js
+++ index.js
@@ -4,17 +4,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const BE_data_json_1 = __importDefault(require("./JSON/BE_data.json"));
-const myEvents = BE_data_json_1.default["Events"];
-function findMostLikelyResults(eventList) {
+console.log(BE_data_json_1.default);
+/*const myEvents = events["Events"];
+
+function findMostLikelyResults(eventList, numberOfResultsToReturn: number) {
+    if(numberOfResultsToReturn<=0) return "You should pick at least one result"
     eventList.sort(function compareFn(a, b) {
         const aHigh = Math.max(a.probability_away_team_winner, a.probability_draw, a.probability_home_team_winner);
         const bHigh = Math.max(b.probability_away_team_winner, b.probability_draw, b.probability_home_team_winner);
-        if (aHigh > bHigh)
-            return -1;
-        if (bHigh > aHigh)
-            return 1;
+        if (aHigh > bHigh) return -1;
+        if (bHigh > aHigh) return 1;
     });
-    return eventList.slice(0, 10);
+    
+    return eventList.slice(0,numberOfResultsToReturn);
 }
-console.log(findMostLikelyResults(myEvents));
+console.log(findMostLikelyResults(myEvents, 1));*/
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,13 +1,17 @@
 import events from './JSON/BE_data.json';
-const myEvents = events["Events"];
-function findMostLikelyResults(eventList) {
+console.log(events);
+/*const myEvents = events["Events"];
+
+function findMostLikelyResults(eventList, numberOfResultsToReturn: number) {
+    if(numberOfResultsToReturn<=0) return "You should pick at least one result"
     eventList.sort(function compareFn(a, b) {
         const aHigh = Math.max(a.probability_away_team_winner, a.probability_draw, a.probability_home_team_winner);
         const bHigh = Math.max(b.probability_away_team_winner, b.probability_draw, b.probability_home_team_winner);
         if (aHigh > bHigh) return -1;
         if (bHigh > aHigh) return 1;
     });
-    return eventList.slice(0,10);
+    
+    return eventList.slice(0,numberOfResultsToReturn);
 }
-console.log(findMostLikelyResults(myEvents));
+console.log(findMostLikelyResults(myEvents, 1));*/
 
