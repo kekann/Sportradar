3aca1c03dd57a1bc62b09aae32e242b284a9b4a0
Jan Zenger <janek.zeng@gmail.com>
2023-01-22 11:14:16 +0100

    add gitignore, function formatting output

--- index.js
+++ index.js
@@ -1,22 +1,50 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const BE_data_json_1 = __importDefault(require("./JSON/BE_data.json"));
-console.log(BE_data_json_1.default);
-/*const myEvents = events["Events"];
-
-function findMostLikelyResults(eventList, numberOfResultsToReturn: number) {
-    if(numberOfResultsToReturn<=0) return "You should pick at least one result"
-    eventList.sort(function compareFn(a, b) {
-        const aHigh = Math.max(a.probability_away_team_winner, a.probability_draw, a.probability_home_team_winner);
-        const bHigh = Math.max(b.probability_away_team_winner, b.probability_draw, b.probability_home_team_winner);
-        if (aHigh > bHigh) return -1;
-        if (bHigh > aHigh) return 1;
-    });
-    
-    return eventList.slice(0,numberOfResultsToReturn);
+var events = require("./JSON/BE_data.json");
+var myEvents = events["Events"];
+function getResultIndicatorString(highestProbResult) {
+    switch (highestProbResult) {
+        case 'probability_home_team_winner':
+            return 'HOME_TEAM_WIN';
+        case 'probability_away_team_winner':
+            return 'AWAY_TEAM_WIN';
+        case 'probability_draw':
+            return 'DRAW';
+        default:
+            return 'Invalid variable name';
+    }
 }
-console.log(findMostLikelyResults(myEvents, 1));*/
-//# sourceMappingURL=index.js.map
+function formatResultsToDisplay(arrayOfEvents, numberOfResultsToFormat) {
+    var formatedResults = '';
+    var _loop_1 = function (i) {
+        var startDate = new Date(arrayOfEvents[i].start_date);
+        var competitorA = arrayOfEvents[i].competitors[0].name + " (" + arrayOfEvents[i].competitors[0].country + ") vs. ";
+        var competitorB = arrayOfEvents[i].competitors[1].name + " (" + arrayOfEvents[i].competitors[1].country + ")\n";
+        var stadion = "Venue: " + arrayOfEvents[i].venue.name + "\n";
+        var highestProbability = Math.max(arrayOfEvents[i].probability_away_team_winner, arrayOfEvents[i].probability_draw, arrayOfEvents[i].probability_home_team_winner);
+        var highestProbResult = Object.entries(arrayOfEvents[i]).find(function (_a) {
+            var key = _a[0], value = _a[1];
+            return value === highestProbability;
+        })[0];
+        formatedResults += "Start date: " + startDate.toLocaleString() + '\n' + competitorA + competitorB + stadion + "Highest probable result : " + getResultIndicatorString(highestProbResult) + " (" + highestProbability + ")\n";
+    };
+    for (var i = 0; i < numberOfResultsToFormat; i++) {
+        _loop_1(i);
+    }
+    ;
+    return formatedResults;
+}
+function findMostLikelyResults(eventList, numberOfResultsToReturn) {
+    if (numberOfResultsToReturn <= 0)
+        return "You should pick at least one result";
+    eventList.sort(function compareFn(a, b) {
+        var aHigh = Math.max(a.probability_away_team_winner, a.probability_draw, a.probability_home_team_winner);
+        var bHigh = Math.max(b.probability_away_team_winner, b.probability_draw, b.probability_home_team_winner);
+        if (aHigh > bHigh)
+            return -1;
+        if (bHigh > aHigh)
+            return 1;
+    });
+    var slicedEvents = eventList.slice(0, numberOfResultsToReturn);
+    var tempFormated = formatResultsToDisplay(slicedEvents, numberOfResultsToReturn);
+    return tempFormated;
+}
+console.log(findMostLikelyResults(myEvents, 10));
--- index.ts
+++ index.ts
@@ -1,17 +1,44 @@
-import events from './JSON/BE_data.json';
-console.log(events);
-/*const myEvents = events["Events"];
+const events = require("./JSON/BE_data.json");
+const myEvents = events["Events"];
+
+function getResultIndicatorString(highestProbResult: string): string {
+    switch (highestProbResult) {
+        case 'probability_home_team_winner':
+            return 'HOME_TEAM_WIN';
+        case 'probability_away_team_winner':
+            return 'AWAY_TEAM_WIN';
+        case 'probability_draw':
+            return 'DRAW';
+        default:
+            return 'Invalid variable name';
+    }
+}
+
+function formatResultsToDisplay(arrayOfEvents, numberOfResultsToFormat: number): string{
+    let formatedResults = ''; 
+    for (let i = 0; i < numberOfResultsToFormat; i++){
+        let startDate = new Date(arrayOfEvents[i].start_date);
+        let competitorA = arrayOfEvents[i].competitors[0].name + " (" +arrayOfEvents[i].competitors[0].country + ") vs. ";
+        let competitorB = arrayOfEvents[i].competitors[1].name + " (" +arrayOfEvents[i].competitors[1].country + ")\n";
+        let stadion = "Venue: " + arrayOfEvents[i].venue.name + "\n";
+        let highestProbability = Math.max(arrayOfEvents[i].probability_away_team_winner, arrayOfEvents[i].probability_draw, arrayOfEvents[i].probability_home_team_winner);
+        let highestProbResult = Object.entries(arrayOfEvents[i]).find(([key, value]) => value === highestProbability)[0];
+        formatedResults += "Start date: " + startDate.toLocaleString() + '\n' + competitorA + competitorB + stadion + "Highest probable result : " + getResultIndicatorString(highestProbResult) + " (" + highestProbability + ")\n";
+    };
+    return formatedResults;
+}
 
 function findMostLikelyResults(eventList, numberOfResultsToReturn: number) {
-    if(numberOfResultsToReturn<=0) return "You should pick at least one result"
+    if(numberOfResultsToReturn<=0) return "You should pick at least one result";
     eventList.sort(function compareFn(a, b) {
         const aHigh = Math.max(a.probability_away_team_winner, a.probability_draw, a.probability_home_team_winner);
         const bHigh = Math.max(b.probability_away_team_winner, b.probability_draw, b.probability_home_team_winner);
         if (aHigh > bHigh) return -1;
         if (bHigh > aHigh) return 1;
     });
-    
-    return eventList.slice(0,numberOfResultsToReturn);
+    const slicedEvents = eventList.slice(0,numberOfResultsToReturn);
+    const tempFormated = formatResultsToDisplay(slicedEvents, numberOfResultsToReturn);
+    return tempFormated;
 }
-console.log(findMostLikelyResults(myEvents, 1));*/
+console.log(findMostLikelyResults(myEvents, 10));
 
